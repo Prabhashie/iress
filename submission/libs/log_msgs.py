8335f02ef5c3c37cf7d89863bc5b4deac4780419
syat002 <syat0002@monash.edu>
2022-08-11 21:55:23 +1000

    adds logging capability

---
+++ submission/libs/log_msgs.py
@@ -0,0 +1,19 @@
+"""
+file name:      errors.py
+author:         Sachinthana Pathiranage
+date created:   11/08/2022
+purpose:        Contains the logger messages
+version:        1.0
+"""
+
+# ERR
+UNEXPECTED_TYPE_ERR = "ERR: Unexpected Type for Coordinate."
+INVALID_DIRECTION_ERR = "ERR: Invalid Direction."
+INVALID_COORD_RANEG_ERR = "ERR: Invalid Range for Coordinates."
+INVALID_ATTR_COUNT_ERR = "ERR: Invalid Attribute Count."
+INVALID_COMMAND_ERR = "ERR: Invalid Command."
+ROBOT_NOT_PLACED_ERR = "ERR: Robot not yet Placed on the Board."
+INVALID_COMMAND_FORMAT_ERR = "Err: Invalid Command Format."
+
+# INFO
+EXIT_PROGRAM_INFO = "INFO: Program Exit Command Received."
