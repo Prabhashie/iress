81a25f2db8b8cd494bab8547a64f313013e2d894
syat002 <syat0002@monash.edu>
2022-08-13 14:22:06 +1000

    setup package imports

---
+++ submission/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages  
+
+setup(name = 'libs', packages = find_packages())
+setup(name = 'src', packages = find_packages())
+setup(name = 'tests', packages = find_packages())
