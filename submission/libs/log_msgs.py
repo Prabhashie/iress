eee89c4528665032cf77e6f39bc72ac8b6cc441c
syat002 <syat0002@monash.edu>
2022-08-13 16:34:43 +1000

    adds more log messages

--- submission/libs/log_msgs.py
+++ submission/libs/log_msgs.py
@@ -7,6 +7,8 @@ version:        1.0
 """
 
 # ERR
+INVALID_CHOICE_ERR = "ERR: Invalid choice. Please enter the number for the given options."
+INVALID_DIMENSION_ERR = "ERR: Grid dimension should be integer."
 UNEXPECTED_TYPE_ERR = "ERR: Unexpected type for coordinate."
 INVALID_DIRECTION_ERR = "ERR: Invalid direction."
 INVALID_COORD_RANEG_ERR = "ERR: Invalid range for coordinates."
@@ -16,4 +18,5 @@ ROBOT_NOT_PLACED_ERR = "ERR: Robot not yet placed on the board."
 INVALID_COMMAND_FORMAT_ERR = "Err: Invalid command format."
 
 # INFO
+NEW_GRID_DIMENSIONS_SET_INFO = "INFO: Successfully updated new grid dimensions."
 EXIT_PROGRAM_INFO = "INFO: Program exit command received."
