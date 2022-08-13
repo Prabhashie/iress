3d512bfd92a2db1806a1f90d6c5152667356f18e
syat002 <syat0002@monash.edu>
2022-08-13 15:45:24 +1000

    adds file and function header documentation

--- submission/src/raw_functionality.py
+++ submission/src/raw_functionality.py
@@ -1,22 +1,106 @@
+"""
+file name:      raw_functionality.py
+author:         Sachinthana Pathiranage
+date created:   09/08/2022
+purpose:        Contains the raw functionality for the requirement. 
+                This implimentation doesn't follow OOP concepts.
+version:        1.0
+"""
+
+# imports
 from libs.log_msgs import *
 
+# constants
 DIRECTIONS = ["NORTH", "SOUTH", "EAST", "WEST"]
 COMMANDS = ["PLACE", "MOVE", "LEFT", "RIGHT", "REPORT"]
 GRID_X = 5
 GRID_Y = 5
 
 def getProcessedInput():
+    """Processes the initial input command.
+
+    :return: List of constituents of the command.
+    :rtype: list
+    """
     comm = input("")
-    comm = comm.strip().split(" ") # remove leading or trailing whitespaces
+    comm = comm.strip().split(" ", 1) # split on first occurence to avoid spaces in args of "PLACE" command
+    comm = [c.strip() for c in comm if c != ""] # remove leading or trailing whitespaces
 
     return comm
 
+def getProcessedArgs(args):
+    """Processes the arguments for PLACE command.
+
+    :param args: Args for the PLACE command.
+    :type args: str
+    :return: List of constituent args.
+    :rtype: list
+    """
+    newArgs = args.strip().split(',')
+    newArgs = [a.strip() for a in newArgs if a != "" and not a.isspace()] # remove leading or trailing whitespaces and empty args
+    print(newArgs)
+
+    return newArgs
+
+def validateArgs(args):
+    """Validate args of the PLACE command.
+
+    :param args: Args for the PLACE command.
+    :type args: list
+    :return: ifValidArgs, errorString, x coordinate, y coordinate, direction. For invalid args, last three args will be of None type.
+    :rtype: bool, str, int, int, str
+    """
+    errStr = ""
+    x, y, f = None, None, None
+
+    try:
+        x = int(args[0])
+    except:
+        errStr = UNEXPECTED_TYPE_ERR + " x coordinate should be an integer."
+        return False, errStr, x, y, f
+
+    # vallidate y is number
+    try:
+        y = int(args[1])
+    except:
+        errStr = UNEXPECTED_TYPE_ERR + " y coordinate should be an integer."
+        return False, errStr, x, y, f
+
+    # validate face
+    if args[2].upper() in DIRECTIONS:
+        f = args[2].upper()
+    else:
+        errStr = INVALID_DIRECTION_ERR + " Valid directions are [NORTH, SOUTH, EAST, WEST]."
+        return False, errStr, x, y, f
+    
+    return True, errStr, x, y, f
+
 def validCoords(x, y):
+    """Validate x, y coordinate range.
+
+    :param x: y coordinate.
+    :type x: int
+    :param y: y coordinate.
+    :type y: int
+    :return: If coordinates are valid.
+    :rtype: bool
+    """
     if x < GRID_X and y < GRID_Y:
         return True
     return False
 
-def getNewCoords(x, y, f):
+def move(x, y, f):
+    """Move the robot.
+
+    :param x: y coordinate.
+    :type x: int
+    :param y: y coordinate.
+    :type y: int
+    :param f: Facing direction.
+    :type f: str
+    :return: New x and y coordinates.
+    :rtype: int, int
+    """
     newX, newY = x, y
     if f == "NORTH":
         newY = y + 1
@@ -29,25 +113,34 @@ def getNewCoords(x, y, f):
     
     return newX, newY
 
-def getNewDirection(f, comm):
+def turn(f, direction):
+    """Turn the robot.
+
+    :param f: Current facing direction.
+    :type f: str
+    :param direction: Turning direction.
+    :type direction: str
+    :return: New direction.
+    :rtype: str
+    """
     newF = f
     if f == "NORTH":
-        if comm == "LEFT":
+        if direction == "LEFT":
             newF = "WEST"
         else:
             newF = "EAST"
     elif f == "SOUTH":
-        if comm == "LEFT":
+        if direction == "LEFT":
             newF = "EAST"
         else:
             newF = "WEST"
     elif f == "EAST":
-        if comm == "LEFT":
+        if direction == "LEFT":
             newF = "NORTH"
         else:
             newF = "SOUTH"
     else:
-        if comm == "LEFT":
+        if direction == "LEFT":
             newF = "SOUTH"
         else:
             newF = "NORTH"
@@ -55,6 +148,8 @@ def getNewDirection(f, comm):
     return newF
 
 def solution():
+    """Driver functionality for the toy robot.
+    """
     robotPlaced = False
     x, y, f = None, None, None
 
@@ -63,29 +158,13 @@ def solution():
 
         if len(comm) == 2: # PLACE command expected
             if comm[0].upper() == 'PLACE':
-                directions = comm[1].strip().split(',')
-                if len(directions) == 3: # if there are 3 parts in the later part of the command
+                args = getProcessedArgs(comm[1])
+                if len(args) == 3: # if there are 3 args in the later part of the command
                     # validate x is number
-                    try:
-                        x = int(directions[0])
-                    except:
-                        print(UNEXPECTED_TYPE_ERR)
+                    validArgs, errStr, x, y, f = validateArgs(args)
+                    if not validArgs:
+                        print(errStr)
                         continue
-
-                    # vallidate y is number
-                    try:
-                        y = int(directions[1])
-                    except:
-                        print(UNEXPECTED_TYPE_ERR)
-                        continue
-
-                    # validate face
-                    if directions[2].upper() in DIRECTIONS:
-                        f = directions[2].upper()
-                    else:
-                        print(INVALID_DIRECTION_ERR)
-                        continue
-                    
                     # validate coordinates within the board
                     if validCoords(x,y):
                         robotPlaced = True
@@ -107,11 +186,11 @@ def solution():
                 continue
             else: # for correct single word commands
                 if comm[0].upper() == "MOVE":
-                    newX, newY = getNewCoords(x, y, f) # get new coords if moved
+                    newX, newY = move(x, y, f) # get new coords if moved
                     if validCoords(newX, newY): # validate coordinates within the board
                         x, y = newX, newY
                 elif comm[0].upper() == "LEFT" or comm[0].upper() == "RIGHT":
-                    f = getNewDirection(f, comm[0].upper()) # get new direction after turning
+                    f = turn(f, comm[0].upper()) # get new direction after turning
                 elif comm[0].upper() == "REPORT":
                     print(f"Output: {x}, {y}, {f}")
                 else:
