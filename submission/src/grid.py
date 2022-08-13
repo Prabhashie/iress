26f1d0c8885aec08a78ab7930dfbf04c84ee3083
syat002 <syat0002@monash.edu>
2022-08-13 16:34:28 +1000

    renames board class to grid

--- submission/src/grid.py
+++ submission/src/grid.py
@@ -1,15 +1,15 @@
 """
-file name:      board.py
+file name:      grid.py
 author:         Sachinthana Pathiranage
 date created:   11/08/2022
-purpose:        Class definition for the board the robot traverses
+purpose:        Class definition for the grid, the robot traverses
 version:        1.0
 """
 # imports
 
-class Board:
-    """Board class on which the board moves. 
-    Once an object is created, it can be used specify the dimensions of the board the robot moves.
+class Grid:
+    """Grid class on which the robot moves. 
+    Once an object is created, it can be used specify the dimensions of the grid, the robot moves.
     If xDim and yDim are not specified, default grid will be 5X5.
 
     :param xDim: Grid size - x dimension
