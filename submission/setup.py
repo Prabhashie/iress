deb3c463bbeef9ad1bd8dc9c221bc118721c7772
syat002 <syat0002@monash.edu>
2022-08-14 17:55:00 +1000

    deletes setup file

--- submission/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages  
-
-setup(name = 'utils', packages = find_packages())
-setup(name = 'classes', packages = find_packages())
-setup(name = 'src', packages = find_packages())
-setup(name = 'tests', packages = find_packages())
