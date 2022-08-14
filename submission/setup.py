28bbad1c180387c2d2c238acf76d0ff51e72cee7
syat002 <syat0002@monash.edu>
2022-08-14 12:07:29 +1000

    updates setup.py

--- submission/setup.py
+++ submission/setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 setup(name = 'libs', packages = find_packages())
 setup(name = 'src', packages = find_packages())
-setup(name = 'tests', packages = find_packages())
+setup(name = 'classes', packages = find_packages())
