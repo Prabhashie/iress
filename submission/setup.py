64c7f7f68e00bade911a153aa0948f14e68782ac
syat002 <syat0002@monash.edu>
2022-08-14 17:22:21 +1000

    removes all init files

--- submission/setup.py
+++ submission/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages  
 
 setup(name = 'utils', packages = find_packages())
-setup(name = 'src', packages = find_packages())
 setup(name = 'classes', packages = find_packages())
+setup(name = 'src', packages = find_packages())
+setup(name = 'tests', packages = find_packages())
