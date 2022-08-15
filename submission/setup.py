921005823f34714edb56f50ff0ef05910a1ae119
syat002 <syat0002@monash.edu>
2022-08-15 20:47:15 +1000

    adds documentation to setup file

--- submission/setup.py
+++ submission/setup.py
@@ -1,3 +1,12 @@
+"""
+file name:      setup.py
+author:         Sachinthana Pathiranage
+date created:   11/08/2022
+purpose:        Setup package imports
+version:        1.0
+"""
+
+# imports
 from setuptools import setup, find_packages  
 
 setup(name = 'utils', packages = find_packages())
